3f1edbea411600cb6af54cd4195a090071313874
Kurry Tran <kurry.tran@gmail.com>
2011-10-25 03:51:35 -0400

    Initial Commit

---
+++ StoryboardUITableView/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  StoryboardUITableView
+//
+//  Created by Kurry Tran on 10/25/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
